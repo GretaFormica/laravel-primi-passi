d0692451341f84bec74228bb7fdcb08780040434
GretaFormica <greta.formica@gmail.com>
2023-03-27 17:16:14 +0200

    saluto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $soggetto = 'World';
+    return view('home', compact('soggetto'));
 });
